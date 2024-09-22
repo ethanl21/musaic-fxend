2f139396dd64f92ce96e838680634fa598d1e92c
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-09-22 13:56:16 -0700

    Add ForgotPasswordForm component

---
+++ app/(auth-pages)/forgot-password/(components)/ForgotPasswordForm.tsx
@@ -0,0 +1,38 @@
+import { forgotPasswordAction } from "@/app/actions";
+import { FormMessage, Message } from "@/components/form-message";
+import { SubmitButton } from "@/components/submit-button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export const ForgotPasswordForm = ({ searchParams }: { searchParams: Message }) => {
+	return (
+		<>
+			<form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-96 max-w-96 mx-auto">
+				<div>
+					<h1 className="">Reset Password</h1>
+				</div>
+				<div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+					<Label htmlFor="email">Email</Label>
+					<Input name="email" placeholder="you@example.com" required />
+					<div className="flex flex-row space-x-2 justify-end">
+						<Link
+							className={buttonVariants({ variant: "default" })}
+							href="/sign-in"
+						>
+							Back
+						</Link>
+						<SubmitButton
+							formAction={forgotPasswordAction}
+							className={buttonVariants({ variant: "destructive" })}
+						>
+							Reset Password
+						</SubmitButton>
+					</div>
+					<FormMessage message={searchParams} />
+				</div>
+			</form>
+		</>
+	);
+};
