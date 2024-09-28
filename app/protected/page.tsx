9bccfdb8720b29799f649c43581543d602d78eee
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-09-28 15:40:21 -0700

    Remove template assets

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,4 +1,3 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
@@ -29,10 +28,7 @@ export default async function ProtectedPage() {
 					{JSON.stringify(user, null, 2)}
 				</pre>
 			</div>
-			<div>
-				<h2 className="font-bold text-2xl mb-4">Next steps</h2>
-				<FetchDataSteps />
-			</div>
+			
 		</div>
 	);
 }
